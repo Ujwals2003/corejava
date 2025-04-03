519d145b005b1eb576d39df438f8a7518a7698b6
ujwals2003 <ujwalskumar8@gmail.com>
2025-04-04 01:21:32 +0530

    8 commit

---
+++ BlackAndWhite/src/Color/BlackAndWhiteRunner.java
@@ -0,0 +1,9 @@
+package Color;
+
+public class BlackAndWhiteRunner {
+    public static void main(String[] args) {
+        BlackAndWhite bw = new BlackAndWhite("Monochrome", "High Contrast");
+        Photography photo = new Photography("Ansel Adams", bw);
+        photo.displayPhotoDetails();
+    }
+}
