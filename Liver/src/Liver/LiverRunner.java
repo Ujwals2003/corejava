ff503b0629efe959058a83ed343aa64aaf57ed44
ujwals2003 <ujwalskumar8@gmail.com>
2025-04-04 02:52:41 +0530

    12 commit

---
+++ Liver/src/Liver/LiverRunner.java
@@ -0,0 +1,9 @@
+package Liver;
+
+public class LiverRunner {
+    public static void main(String[] args) {
+        Liver liver = new Liver("Detoxification", "Healthy");
+        LiverCheckup checkup = new LiverCheckup("Dr. Sharma", liver);
+        checkup.displayCheckup();
+    }
+}
