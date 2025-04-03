652126a8cc714683aaeba2332cd2db841b3d7582
ujwals2003 <ujwalskumar8@gmail.com>
2025-04-04 01:11:47 +0530

    7 commit

---
+++ Earth/src/Earth/EarthRunner.java
@@ -0,0 +1,9 @@
+package Earth;
+
+public class EarthRunner {
+    public static void main(String[] args) {
+        Earth earth = new Earth("Terrestrial", 4);
+        Planet planet = new Planet("Milky Way", earth);
+        planet.planetInfo();
+    }
+}
